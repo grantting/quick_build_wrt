d27fa2198fbbcff454de471bd8d33d75749a26ce
ding <dhq.s@qq.com>
2024-11-29 18:47:12 +0800

    2.9.1

--- build/building_image.py
+++ build/building_image.py
@@ -34,7 +34,7 @@ if __name__ == "__main__":
     files = "files"
     
     # 读取插件列表
-    filename = 'packages.txt'
+    filename = 'external-package.txt'
     packages = read_packages_from_file(filename)
 
     # 构建并打印命令
