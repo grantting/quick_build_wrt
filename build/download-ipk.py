613f57c4a404f33dcbaa588f70833871277e6f25
ding <dhq.s@qq.com>
2024-12-17 12:46:17 +0800

    3.1.2

--- build/download-ipk.py
+++ build/download-ipk.py
@@ -25,7 +25,6 @@ if not platform:
 source_urls = [
     f"https://dl.openwrt.ai/releases/24.10/packages/{arch_packages}/kiddin9/Packages.gz",
     f"https://downloads.immortalwrt.org/releases/23.05.4/packages/{arch_packages}/luci/Packages.gz",
-    f"https://mirror-03.infra.openwrt.org/releases/23.05.5/packages/{arch_packages}/base/Packages.gz",
 ]
 
 # 创建目录来存放下载和解压的文件
